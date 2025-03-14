ee9e95ed11003c9645c4c5d54427f85d1469b379
PabloMyLove <94645033+PabloMyDarling@users.noreply.github.com>
2025-03-14 21:26:08 +0300

    Delete lua/plugins/telescope.lua

--- lua/plugins/telescope.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep', 'sharkdp/fd', 'nvim-treesitter/nvim-treesitter',
-			 'nvim-tree/nvim-web-devicons'
-	}
-}
